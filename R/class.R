07fad6edf6270ada263ee61afe7ec3a47ea54dd2
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2020-08-25 15:23:14 +0200

    add validator r2sls class

--- R/class.R
+++ R/class.R
@@ -20,3 +20,34 @@ new_r2sls <- function(x = list()) {
   structure(x, class = "r2sls")
 
 }
+
+#' Validator of r2sls class
+#'
+#' \code{validate_2rsls} checks that the input is a valid object of \link{class}
+#' "\code{r2sls}"
+#'
+#' @param x An object whose validity of class "\code{r2sls}" is tested.
+
+validate_r2sls <- function(x) {
+
+  values <- unclass(x)
+
+  if (!is.data.frame(x[["data"]]) | !is.matrix(x[["data"]])) {
+    stop("Component `data` must either be a dataframe or matrix", call. = FALSE)
+  }
+  if (!(x[["gamma"]] > 0 & x[["gamma"]] < 1)) {
+    stop("Component `gamma` must be between 0 and 1", call. = FALSE)
+  }
+  if (!is.numeric(x[["coefficients"]])) {
+    stop("Component `coefficients` must be numeric", call. = FALSE)
+  }
+  if (!is.logical(x[["converged"]]) | !is.null(x[["converged"]])) {
+    stop("Component `coefficients` must be logical or NULL", call. = FALSE)
+  }
+  if(!is.integer(x[["iterations"]]) | x[["integers"]] < 0) {
+    stop("Component `iterations`must be integer >= 0", call. = FALSE)
+  }
+
+  x
+
+}
