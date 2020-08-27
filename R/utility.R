fe4946d5bf9711bd90abca03130587e89204e944
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2020-08-27 12:28:39 +0200

    change naming convention, x1 is now exogenous and x2 endogenous (before was opposite)

--- R/utility.R
+++ R/utility.R
@@ -1,18 +1,19 @@
 #' Extract the elements of ivreg formula
 #'
 #' \code{extract_formula} takes a formula object for \code{ivreg}, i.e. in a
-#' format of \code{y ~ x | z} and extracts the different elements in a list.
-#' Each element is a character vector storing the different types of regressors.
-#' Element \code{y_var} refers to the dependent variable, \code{x1_var} to the
-#' endogenous regressors, \code{x2_var} and \code{z1_var} to the exogenous
-#' regressors (which have to be included as instruments), and \code{z2_var} to
-#' the outside instruments.
+#' format of \code{y ~ x1 + x2 | z1 + z2} and extracts the different elements
+#' in a list. Each element is a character vector storing the different types of
+#' regressors. Element \code{y_var} refers to the dependent variable,
+#' \code{x1_var} to the exogenous regressors, \code{x2_var} to the endogenous
+#' regressors, \code{z1_var} to the exogenous regressors (which have to be
+#' included again as instruments and hence coincide with \code{x1_var}), and
+#' \code{z2_var} refers to the outside instruments.
 #'
 #' @param formula A formula for the \code{\link[AER]{ivreg}} function, i.e. in
 #' format \code{y ~ x | z}.
 #'
 #' @examples
-#' extract_formula(y ~ x1 + x2 + x3 | x2 + x3 + z4)
+#' extract_formula(y ~ x1 + x2 + x3 | x1 + x2 + z3)
 
 extract_formula <- function(formula) {
 
@@ -26,7 +27,7 @@ extract_formula <- function(formula) {
   # ensure that the formula contains three parts
   if (length(fml_split[[1]]) != 3) {
     stop(strwrap("The `formula` does not consist of three parts as in
-         y ~ x1 + x2 | x2 + z3", prefix = " ", initial = ""))
+         y ~ x1 + x2 | z1 + z2", prefix = " ", initial = ""))
   }
 
   # delete symbols and leading & trailing spaces, collect in character vector
@@ -36,12 +37,12 @@ extract_formula <- function(formula) {
   z_var <- fml_split[[1]][3]
   z_var <- trimws(strsplit(z_var, "\\+")[[1]])
 
-  x1_var <- setdiff(x_var, z_var) # endogenous regressors
-  x2_var <- setdiff(x_var, x1_var) # exogenous regressors
-  z1_var <- x2_var # included instruments
+  x1_var <- setdiff(x_var, x2_var) # exogenous regressors
+  x2_var <- setdiff(x_var, z_var) # endogenous regressors
+  z1_var <- x1_var # included instruments
   z2_var <- setdiff(z_var, z1_var) # outside instruments
 
-  if (length(x1_var) > length(z2_var)) {
+  if (length(x2_var) > length(z2_var)) {
     stop(strwrap("The specified formula does not fulfill the order condition
       (for 2SLS, the number of outside instruments must be weakly larger than
       the number of endogenous regressors", prefix = " ", initial = ""))
