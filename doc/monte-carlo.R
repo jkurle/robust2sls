f028b256ed74cca7fe30c311dd3a151925278e4c
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2021-11-10 15:20:02 +0000

    update vignette for GH Actions

--- doc/monte-carlo.R
+++ doc/monte-carlo.R
@@ -14,18 +14,23 @@ p <- generate_param(dx1 = 3, dx2 = 2, dz2 = 3, intercept = TRUE, seed = 10)
 ## -----------------------------------------------------------------------------
 library(parallel)
 ncores <- min(max(detectCores() - 1, 1), 2)
-cl <- makeCluster(ncores)
 
 ## -----------------------------------------------------------------------------
 library(doParallel)
+cl <- makeCluster(ncores)
 registerDoParallel(cl)
+
+# export libraries to all workers in the cluster
+invisible(clusterCall(cl = cl, function(x) .libPaths(x), .libPaths()))
 sim1 <- mc_grid(M = 100, n = c(100, 1000), seed = 42, parameters = p, 
                formula = p$setting$formula, ref_dist = "normal", 
                sign_level = 0.05, initial_est = "robustified", iterations = 0,
                shuffle = FALSE, shuffle_seed = 42, split = 0.5)
+stopCluster(cl)
 
 ## -----------------------------------------------------------------------------
 library(doFuture)
+cl <- makeCluster(ncores)
 registerDoFuture()
 plan(cluster, workers = cl)
 sim2 <- mc_grid(M = 100, n = c(100, 1000), seed = 42, parameters = p, 
