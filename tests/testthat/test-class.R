e485940199d4c832d76f49c96a80ebce43e0cea5
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2021-11-29 11:30:24 +0000

    add tests for new argument max_iter

--- tests/testthat/test-class.R
+++ tests/testthat/test-class.R
@@ -483,6 +483,16 @@ test_that("validate_robust2sls() works correctly", {
   expect_error(validate_robust2sls(t),
                "Element m0 of list \\$type must be a numeric vector that only contains the values -1, 0, or 1")
 
+  # new tests for max_iter argument (test3 used convergence)
+  t <- test3
+  t$cons$convergence$max_iter <- "5"
+  expect_error(validate_robust2sls(t),
+               "x\\$cons\\$convergence\\$max_iter must either be NULL or numeric")
+  z <- test1 # test1 sets numeric iteration
+  z$cons$convergence$max_iter <- 3
+  expect_error(validate_robust2sls(z),
+               "When iterations is numeric, then max_iter must be NULL")
+
   skip_on_cran()
   expect_snapshot_output(test1)
   expect_snapshot_output(test2)
