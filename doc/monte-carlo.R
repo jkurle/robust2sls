dd269222de326ecb8ac16c4260fbf469ad9d4712
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2026-08-13 15:22:56 +0200

    make monte carlo vignette more robust
    Fixes #19

--- doc/monte-carlo.R
+++ doc/monte-carlo.R
@@ -1,14 +1,14 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- include = FALSE, echo = FALSE-------------------------------------------
+## ----include = FALSE, echo = FALSE--------------------------------------------
 Sys.setenv(LANGUAGE="en")
 
-## ---- eval = FALSE------------------------------------------------------------
-#  utils::vignette("overview", package = "robust2sls")
+## ----eval = FALSE-------------------------------------------------------------
+# utils::vignette("overview", package = "robust2sls")
 
 ## -----------------------------------------------------------------------------
 library(robust2sls)
@@ -16,31 +16,38 @@ p <- generate_param(dx1 = 3, dx2 = 2, dz2 = 3, intercept = TRUE, seed = 10)
 
 ## -----------------------------------------------------------------------------
 library(parallel)
-ncores <- 2
-cl <- makeCluster(ncores)
-# export libraries to all workers in the cluster
-invisible(clusterCall(cl = cl, function(x) .libPaths(x), .libPaths()))
-
-## -----------------------------------------------------------------------------
+library(parallelly)
 library(doParallel)
-registerDoParallel(cl)
-sim1 <- mc_grid(M = 100, n = c(100, 1000), seed = 42, parameters = p, 
-               formula = p$setting$formula, ref_dist = "normal", 
-               sign_level = 0.05, initial_est = "robustified", iterations = 0,
-               shuffle = FALSE, shuffle_seed = 42, split = 0.5)
-
-## -----------------------------------------------------------------------------
 library(doFuture)
-registerDoFuture()
-plan(cluster, workers = cl)
-sim2 <- mc_grid(M = 100, n = c(100, 1000), seed = 42, parameters = p, 
-               formula = p$setting$formula, ref_dist = "normal", 
-               sign_level = 0.05, initial_est = "robustified", iterations = 0,
-               shuffle = FALSE, shuffle_seed = 42, split = 0.5)
-stopCluster(cl)
 
-# check identical results
-identical(sim1, sim2)
+ncores <- 2
+cl <- makeClusterPSOCK(ncores)
+
+tryCatch({
+  # export libraries to all workers in the cluster
+  invisible(clusterCall(cl = cl, function(x) .libPaths(x), .libPaths()))
+
+  registerDoParallel(cl)
+  sim1 <- mc_grid(M = 100, n = c(100, 1000), seed = 42, parameters = p,
+                  formula = p$setting$formula, ref_dist = "normal",
+                  sign_level = 0.05, initial_est = "robustified", iterations = 0,
+                  shuffle = FALSE, shuffle_seed = 42, split = 0.5)
+
+  # Next, use doFuture for the parallel loop. Both implementations should
+  # yield the same result.
+  registerDoFuture()
+  plan(cluster, workers = cl)
+  sim2 <- mc_grid(M = 100, n = c(100, 1000), seed = 42, parameters = p,
+                  formula = p$setting$formula, ref_dist = "normal",
+                  sign_level = 0.05, initial_est = "robustified", iterations = 0,
+                  shuffle = FALSE, shuffle_seed = 42, split = 0.5)
+
+  # check identical results
+  identical(sim1, sim2)
+}, finally = {
+  plan(sequential)
+  stopCluster(cl)
+})
 
 ## -----------------------------------------------------------------------------
 library(doFuture)
