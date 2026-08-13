9eaf8081176ee2e434c37ce1ac82e29568cac28c
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2026-08-13 14:13:59 +0200

    clean up creating testdata interactively
    - not to be simply run
    - mc_grid datasets require insertion of save command into mc_grid() function body

--- tests/testthat/create-testdata.R
+++ tests/testthat/create-testdata.R
@@ -1,3 +1,4 @@
+# IMPORTANT: To be used interactively, cannot simply run to re-create the testing data
 # create fixed datasets to test functions that are not about MC/generating data
 library(robust2sls)
 library(testthat)
@@ -31,47 +32,11 @@ d3 <- generate_data(parameters = p3, n = 50)$data
 saveRDS(d3, file = test_path("./testdata/testdata3.rds"))
 # used once in test-iterative_estimators2
 
-# ##### to test generate_data directly
-# ### artificial data IV
-# p4 <- generate_param(3, 2, 3, seed = 42)
-# set.seed(42)
-# d4 <- generate_data(p4, n = 30)
-# saveRDS(d4, file = test_path("./testdata/testdata4.rds"))
-# # used once in test-monte_carlo
-#
-# ### artificial data V
-# p5 <- generate_param(3, 2, 3, seed = 42)
-# set.seed(42)
-# d5 <- generate_data(p5, n = 500000)
-# saveRDS(d5, file = test_path("./testdata/testdata5.rds"))
-# # used once in test-monte_carlo
-
-# ### artificial data VI
-# p6 <- generate_param(1, 1, 1, seed = 40)
-# d6 <- generate_data(parameters = p6, n = 1000)$data
-# saveRDS(d6, file = test_path("./testdata/testdata6.rds"))
-# # used once in test-outlier_test
-
 # data in mc_grid is special, created inside
 # to save, temporarily add a saveRDS() command inside the mc_grid() function
 
-### d1 (line 266)
-saveRDS(d, paste0("tests/testthat/testdata/mcgrid/d1/n", n, "_m", m, ".rds"))
-p <- generate_param(3, 2, 3, sigma = 2, intercept = TRUE, seed = 42)
-results <- mc_grid(100, n = c(100, 1000), seed = 42, parameters = p, formula = p$setting$formula, ref_dist = "normal", sign_level = c(0.01, 0.05), initial_est = c("saturated", "robustified"), iterations = 0, shuffle = FALSE, shuffle_seed = NULL, split = c(0.3, 0.4, 0.5))
-
-### d2 (line 290, 310)
-saveRDS(d, paste0("tests/testthat/testdata/mcgrid/d2/n", n, "_m", m, ".rds"))
-p <- generate_param(dx1 = 2, dx2 = 1, dz2 = 1, seed = 42)
-out <- mc_grid(M = 10, n = c(1000, 10000), seed = 20, parameters = p, formula = y~x1+x2+x3|x1+x2+z3, ref_dist = "normal", sign_level = 0.05, initial_est = "robustified", iterations = "convergence", convergence_criterion = 0)
-
-### d3 (line 339, 346, 353, 362, 370, 378, 407, 424, 445, 452)
-saveRDS(d, paste0("tests/testthat/testdata/mcgrid/d3/n", n, "_m", m, ".rds"))
-p <- generate_param(3, 2, 3, sigma = 2, intercept = TRUE, seed = 42)
-mc_grid(10, n = c(100, 1000), seed = 42, parameters = p, formula = p$setting$formula, ref_dist = "normal", sign_level = c(0.01, 0.05), initial_est = "robustified", iterations = 0, shuffle = FALSE, shuffle_seed = NULL, split = 0.5, verbose = TRUE)
-
 ### d4 new fixture to be used across all tests
-saveRDS(d, paste0("tests/testthat/testdata/mcgrid/d4/n", n, "_m", m, ".rds"))
+# saveRDS(d, paste0("tests/testthat/testdata/mcgrid/d4/n", n, "_m", m, ".rds"))
 p <- generate_param(3, 2, 3, sigma = 2, intercept = TRUE, seed = 42)
 mc_grid(4, n = c(50), seed = 99991, parameters = p, formula = p$setting$formula, ref_dist = "normal", sign_level = c(0.01, 0.05), initial_est = "robustified", iterations = 0, shuffle = FALSE, shuffle_seed = NULL, split = 0.5, verbose = TRUE)
 mc_grid(4, n = c(100), seed = 2389, parameters = p, formula = p$setting$formula, ref_dist = "normal", sign_level = c(0.01, 0.05), initial_est = "robustified", iterations = 0, shuffle = FALSE, shuffle_seed = NULL, split = 0.5, verbose = TRUE)
